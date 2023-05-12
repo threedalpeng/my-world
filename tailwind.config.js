f8fb48ec17ffc2fda870a32b27af7aac3c05feda
Kim Jeong-won <threedalpeng@gmail.com>
2023-05-12 19:53:50 +0900

    design: add theme for daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,5 +14,22 @@ module.exports = {
       },
     },
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#1d4ed8",
+          secondary: "#059669",
+          accent: "#37CDBE",
+          neutral: "#3D4451",
+          "base-100": "#FFFFFF",
+          info: "#3ABFF8",
+          success: "#36D399",
+          warning: "#FBBD23",
+          error: "#F87272",
+        },
+      },
+    ],
+  },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
