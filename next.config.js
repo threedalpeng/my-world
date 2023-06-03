949368a8de7e6e3cbb4438d6ede467f26037e3e4
Kim Jeong-won <threedalpeng@gmail.com>
2023-06-03 10:26:37 +0900

    chore: Remove unnecessary @next/mdx

--- next.config.js
+++ next.config.js
@@ -1,13 +1,16 @@
+const pipe = (functions) => (value) =>
+  functions.reduce(
+    (currentValue, currentFunction) => currentFunction(currentValue),
+    value
+  );
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    mdxRs: true,
-  },
-};
+const nextConfig = {};
 
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
 });
-const withMDX = require("@next/mdx")();
 
-module.exports = withPWA(withMDX(nextConfig));
+const withPlugin = pipe([withPWA]);
+
+module.exports = withPlugin(nextConfig);
